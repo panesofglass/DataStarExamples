77e8caa4706c99684e779254bdc5dd550f917872
Ryan Riley <ryanriley@live.com>
2025-02-05 23:25:10 -0600

    wip: add demo

---
+++ CSharp/MinimalSdk/Models/HomeSignals.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MinimalSdk.Models;
+
+public record HomeSignals
+{
+    [JsonPropertyName("input")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Input { get; init; } = null;
+
+    [JsonPropertyName("output")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Output { get; init; } = null;
+
+    public string Serialize() => JsonSerializer.Serialize(this);
+}
--- CSharp/MinimalSdk/Program.cs
+++ CSharp/MinimalSdk/Program.cs
@@ -1,11 +1,54 @@
+using System.Runtime.CompilerServices;
+using StarFederation.Datastar.DependencyInjection;
+using MinimalSdk.Models;
 using MinimalSdk.Slices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddWebEncoders();
+builder.Services.AddDatastar();
 
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Extensions.RazorSlice<Home, DateTime>(DateTime.Now));
+app.UseStatusCodePages();
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", () => Results.Extensions.RazorSlice<Home>());
+
+var displayDate = false;
+
+app.MapGet("/displayDate", async (IDatastarServerSentEventService sse) =>
+{
+    displayDate = true;
+    while (displayDate)
+    {
+        var today = DateTime.Now.ToString("%y-%M-%d %h:%m:%s");
+        // TODO: use partial rather than explicit string
+        await sse.MergeFragmentsAsync(
+            $"""<div id="target"><span id="date"><b>{today}</b><button data-on-click="@get('/removeDate')">Remove</button></span></div>""");
+        await Task.Delay(1000);
+    }
+});
+
+app.MapGet("/removeDate", async (IDatastarServerSentEventService sse) =>
+{
+    displayDate = false;
+    await sse.RemoveFragmentsAsync("#date");
+});
+
+app.MapPost("/changeOutput", async (IDatastarServerSentEventService sse, IDatastarSignalsReaderService dsSignals) =>
+{
+    var signals = await dsSignals.ReadSignalsAsync<HomeSignals>();
+    var newSignals = new HomeSignals { Output = $"Your input: {signals.Input}" };
+    await sse.MergeSignalsAsync(newSignals.Serialize());
+});
+
+Console.WriteLine($"RuntimeFeature.IsDynamicCodeSupported = {RuntimeFeature.IsDynamicCodeSupported}");
+Console.WriteLine($"RuntimeFeature.IsDynamicCodeCompiled = {RuntimeFeature.IsDynamicCodeCompiled}");
 
 app.Run();
